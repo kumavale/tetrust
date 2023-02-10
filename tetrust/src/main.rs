c0e7f72fa11ea2684089aa6d5d9b5fc67db890ac
kumavale <kumavale.hyperion@gmail.com>
2023-02-10 14:39:31 +0900

    `auto`モード時に「重み(遺伝子配列)」を引数にて指定可能に (#9)

--- tetrust/src/main.rs
+++ tetrust/src/main.rs
@@ -4,7 +4,10 @@ mod play;
 mod ai;
 mod ga;
 
-use clap::{Parser, Subcommand};
+use clap::{
+    Args, Parser, Subcommand,
+    error::{ErrorKind, ContextKind, ContextValue},
+};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -19,9 +22,16 @@ enum Mode {
     /// Run normal play
     Normal,
     /// Run auto play
-    Auto,
+    Auto(Auto),
     /// Learning with GeneticAlgorithm
     Learning,
+}
+
+#[derive(Args)]
+struct Auto {
+    /// Specify gene sequence [default: [100,1,10,100]]
+    #[arg(short, long)]
+    genome: Option<String>,
 }
 
 fn main() {
@@ -33,9 +43,27 @@ fn main() {
             // 通常プレイ
             play::normal();
         }
-        Some(Mode::Auto) => {
+        Some(Mode::Auto(args)) => {
             // オートプレイ
-            play::auto();
+            let genome = match args.genome {
+                Some(genome) => {
+                    genome.trim_matches(|c|!char::is_numeric(c))
+                        .split(|c|!char::is_numeric(c))
+                        .map(|c|c.parse::<u8>().unwrap())
+                        .collect::<Vec<u8>>()
+                        .try_into()
+                        .unwrap_or_else(|_| {
+                            let cmd = clap::Command::new("tetrust");
+                            let mut err = clap::Error::new(ErrorKind::InvalidValue)
+                                .with_cmd(&cmd);
+                            err.insert(ContextKind::InvalidArg, ContextValue::String("--genome".to_owned()));
+                            err.insert(ContextKind::InvalidValue, ContextValue::String(genome));
+                            err.exit();
+                        })
+                }
+                None => [100,1,10,100],
+            };
+            play::auto(genome);
         }
         Some(Mode::Learning) => {
             // 遺伝的アルゴリズムにて学習
